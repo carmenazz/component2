b2a6e03fcd806fb13aa7ef525f021d9c11090fe4
carmenazz <carmen.zorrilla@newhorizons.edu.do>
2021-11-29 18:05:17 +0000

    component2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { BottomComponent } from './bottom/bottom.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    BottomComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
